f111a49b3510149fec098d98cd6390bf7bc4e635
Senatova93 <nanniin160@gmail.com>
2020-04-18 07:48:05 +0300

    myCounter 1 step

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
+import s from './Button.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+      add:[],
+      num: 0,
+      filterValue: "Res"
+  }
+
+
+
+
+    onIncClick = () => {
+      let num = this.state.num
+        num < 5 && num++
+        this.setState({num:num})
+    };
+    onResetClick = () => {
+        this.setState({num:0})
+    };
+    render = () => {
+        debugger
+        let Inc = this.state.filterValue === "Inc" ? "filter-active" : "";
+        let Res = this.state.filterValue === "Res" ? "filter-active" : "";
+
+        return (
+            <div className={style.counterTask}>
+                <div className={style.counter}>
+                    <div className={style.all}>
+                <div className={this.state.num==5?style.red:style.tablo}><span>{this.state.num} </span></div>
+                <div className={style.buttons}>
+                    <div className={style.incriment}>
+                <button onClick = {this.onIncClick} className={s.Inc +' '+ s[Inc]}>inc</button>
+                    </div>
+                    <div className= {style.reset}>
+                <button onClick={this.onResetClick} className={s.Res + ' ' + s[Res]}>reset</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        );
+    }}
+
 
 export default App;
