9053f9bdf74853911fd2105a80506b42cc12bbef
Senatova93 <nanniin160@gmail.com>
2020-04-25 12:56:37 +0300

    myCounter delete .idea

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import style from './App.module.css';
 import s from './Button.module.css'
 
@@ -7,8 +6,20 @@ class App extends React.Component {
   state = {
       add:[],
       num: 0,
-      filterValue: "Res"
+
+      classToggleInc: false,
+      classToggleReset: true,
+      changeButtonStyleInc: true,
+      changeButtonStyleReset: false,
   }
+// changeFilter = (newFilterValue) => {
+//       this.setState({ filterValue:newFilterValue})
+
+// }
+//          changeFilter = () => {
+//       if (state.num = 0) {
+//
+//       }
 
 
 
@@ -17,14 +28,38 @@ class App extends React.Component {
       let num = this.state.num
         num < 5 && num++
         this.setState({num:num})
+        if (this.state.num > -1) {
+            this.setState({
+                classToggleReset: false,
+                changeButtonStyleReset: true,
+            })
+        }
+
+        if (this.state.num === 4) {
+            this.setState({
+                classToggleInc: true,
+                changeButtonStyleInc: false,
+            });
+        }
     };
+
+
+
     onResetClick = () => {
-        this.setState({num:0})
+        this.setState({num:0,
+             classToggleInc: false,
+             classToggleReset: true,
+            changeButtonStyleInc: false,
+            changeButtonStyleReset: true,})
+
+
     };
     render = () => {
-        debugger
-        let Inc = this.state.filterValue === "Inc" ? "filter-active" : "";
-        let Res = this.state.filterValue === "Res" ? "filter-active" : "";
+        let Inc =  this.state.changeButtonStyleInc ? "filter-active" : "";
+        let Res = this.state.changeButtonStyleReset  ? "filter-active" : "";
+
+        // let Inc = this.state.filterValue === "Inc" ? "filter-active" : "";
+        // let Res = this.state.filterValue === "Res" ? "filter-active" : "";
 
         return (
             <div className={style.counterTask}>
@@ -33,10 +68,15 @@ class App extends React.Component {
                 <div className={this.state.num==5?style.red:style.tablo}><span>{this.state.num} </span></div>
                 <div className={style.buttons}>
                     <div className={style.incriment}>
-                <button onClick = {this.onIncClick} className={s.Inc +' '+ s[Inc]}>inc</button>
+                <button
+                    disabled={this.state.classToggleInc}
+                    onClick = {this.onIncClick}
+                    className={s.Inc +' '+ s[Inc]}>inc</button>
                     </div>
                     <div className= {style.reset}>
-                <button onClick={this.onResetClick} className={s.Res + ' ' + s[Res]}>reset</button>
+                <button  disabled={this.state.classToggleReset}
+                    onClick={this.onResetClick}
+                    className={s.Res + ' ' + s[Res]}>reset</button>
                     </div>
                 </div>
                 </div>
